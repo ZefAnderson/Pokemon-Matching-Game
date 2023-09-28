9705072795f2025df6ed77f60c86f6200423b0fe
Jordan <jordan@Jordans-MacBook-Air.local>
2023-09-28 08:33:59 -0600

    environment.ts set up

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDZtXNhKQccMFZCc6BWWcfuWR6g_aq-6Ac",
+        authDomain: "pokemon-matching-game.firebaseapp.com",
+        projectId: "pokemon-matching-game",
+        storageBucket: "pokemon-matching-game.appspot.com",
+        messagingSenderId: "1080178394923",
+        appId: "1:1080178394923:web:419aec4dcfc972c18de76e",
+        measurementId: "G-8TTTEZ601S"
+    }
+}
