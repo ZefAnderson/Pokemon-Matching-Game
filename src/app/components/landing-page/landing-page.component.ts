e6be988db8b4c3eec4bc0e477ff09a74908fcbb2
Howler234 <howler234@gmail.com>
2023-10-03 10:15:12 -0600

    adding Ashlyn changes

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/service/auth.service';
 import { SignUpComponent } from '../sign-up/sign-up.component';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
@@ -10,13 +10,23 @@ import { FormsModule } from '@angular/forms';
 })
 export class LandingPageComponent {
 
- constructor(
+ constructor( 
+  // fb:FormBuilder,
   public authService: AuthService
- ) {}
+ ) {
+  // form: FormGroup;
+  // fb: FormBuilder = new FormBuilder;  // we'll want to be able to access this later outside of the controller
+  //   this.form = fb.group({
+  //     firstName: [''],
+  //     lastName: [''],
+  //     email: [''],
+  //     password: ['']
+  //   });
+  }
 
+  
  submitNewUser() {
 
  };
-
 }
 
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  form: FormGroup;
+  fb: FormBuilder = new FormBuilder;  // we'll want to be able to access this later outside of the controller
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      password: ['']
+    });
+  }
+  submitNewUser() {
+
+  };
 
 }
